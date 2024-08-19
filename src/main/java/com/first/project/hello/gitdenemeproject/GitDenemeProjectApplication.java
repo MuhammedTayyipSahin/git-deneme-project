a57a64a8a53ce3ff5015c40dcb1b744e1a0d80ea
Tayyip Sahin <sahin.tayyip.muhammed@gmail.com>
2024-08-19 15:03:21 +0300

    fisrt commit

---
+++ src/main/java/com/first/project/hello/gitdenemeproject/GitDenemeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.first.project.hello.gitdenemeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDenemeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDenemeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/first/project/hello/gitdenemeproject/GitDenemeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.project.hello.gitdenemeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDenemeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
