07a3e1bfe7221482c090da11b7035cbdde64c138
Tayyip Sahin <sahin.tayyip.muhammed@gmail.com>
2024-08-24 17:13:42 +0300

    Service sınıfı eklendi.

---
+++ src/main/java/com/first/project/hello/gitdenemeproject/MyService.java
@@ -0,0 +1,10 @@
+package com.first.project.hello.gitdenemeproject;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+    public String getUsers() {
+        return "Ali Kulu";
+    }
+}
