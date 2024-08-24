af6fc2cfeb690217a7aa332f72a95fc8a5d86c64
Tayyip Sahin <sahin.tayyip.muhammed@gmail.com>
2024-08-24 21:42:13 +0300

    Service was added.

---
+++ src/main/java/com/first/project/hello/gitdenemeproject/MyService.java
@@ -0,0 +1,10 @@
+package com.first.project.hello.gitdenemeproject;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+    public String  getUser(){
+        return "user1";
+    }
+}
