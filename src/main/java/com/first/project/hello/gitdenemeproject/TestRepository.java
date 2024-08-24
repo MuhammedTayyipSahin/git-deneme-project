788a83c091ccd780c66517ba5a52d0c1a799f123
Tayyip Sahin <sahin.tayyip.muhammed@gmail.com>
2024-08-24 22:31:41 +0300

    Repository was added.

---
+++ src/main/java/com/first/project/hello/gitdenemeproject/TestRepository.java
@@ -0,0 +1,10 @@
+package com.first.project.hello.gitdenemeproject;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TestRepository {
+    public void sayHello(){
+        System.out.println("hello");
+    }
+}
